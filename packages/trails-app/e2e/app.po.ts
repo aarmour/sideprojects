27338ff4ea09f003ee894bbcd668440d73a6ad86
Adam Armour <adam.armour@gmail.com>
2017-12-07 19:58:42 -0700

    chore: init CLI
    packages: trails-app

---
+++ packages/trails-app/e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('trl-root h1')).getText();
+  }
+}
---
+++ packages/trails-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'trl-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'trl';
+}

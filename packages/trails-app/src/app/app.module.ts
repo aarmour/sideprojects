8dcb0be0471a3b32616483b27972109594f08709
Adam Armour <adam.armour@gmail.com>
2017-12-07 20:21:08 -0700

    feat: add Firebase
    packages: trails-app

--- packages/trails-app/src/app/app.module.ts
+++ packages/trails-app/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
@@ -15,7 +17,10 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : []
+    environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : [],
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- packages/trails-app/src/environments/environment.ts
+++ packages/trails-app/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAO10YXtner7a73UgpL9Xrj8X38idp4jOE',
+    authDomain: 'trails-app-b8a2d.firebaseapp.com',
+    databaseURL: 'https://trails-app-b8a2d.firebaseio.com',
+    projectId: 'trails-app-b8a2d',
+    storageBucket: 'trails-app-b8a2d.appspot.com',
+    messagingSenderId: '346388651724'
+  }
 };

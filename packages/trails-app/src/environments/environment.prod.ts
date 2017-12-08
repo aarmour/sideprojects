1d150a4386705a37cae2d5f20c46123cea7c171e
Adam Armour <adam.armour@gmail.com>
2017-12-08 08:15:24 -0700

    build: update @angular/cli and configure prod build
    packages: trails-app

--- packages/trails-app/src/environments/environment.prod.ts
+++ packages/trails-app/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAO10YXtner7a73UgpL9Xrj8X38idp4jOE',
+    authDomain: 'trails-app-b8a2d.firebaseapp.com',
+    databaseURL: 'https://trails-app-b8a2d.firebaseio.com',
+    projectId: 'trails-app-b8a2d',
+    storageBucket: 'trails-app-b8a2d.appspot.com',
+    messagingSenderId: '346388651724'
+  }
 };

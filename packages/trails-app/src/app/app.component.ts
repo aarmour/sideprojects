ae17c225dd7e6b9acc6a77ae0590bc772bf983e4
Adam Armour <adam.armour@gmail.com>
2017-12-08 20:21:21 -0700

    feat(app): snek
    packages: trails-app

--- packages/trails-app/src/app/app.component.ts
+++ packages/trails-app/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { snek } from './snek';
+
 @Component({
   selector: 'trl-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'trl';
+
+  constructor() {
+    console.log(snek);
+  }
+
 }

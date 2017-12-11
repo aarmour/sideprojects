4b8ff811c82d30b6d6bde946fd711bd131604f2c
Adam Armour <adam.armour@gmail.com>
2017-12-10 20:38:47 -0700

    build: fix staging build and deployment
    packages: trails-app

---
+++ packages/trails-app/src/environments/environment.staging.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBAEnzMUL4HDvcfHu7DzrVkTUQ9nv8Zvnk',
+    authDomain: 'trails-app-staging.firebaseapp.com',
+    databaseURL: 'https://trails-app-staging.firebaseio.com',
+    projectId: 'trails-app-staging',
+    storageBucket: 'trails-app-staging.appspot.com',
+    messagingSenderId: '967390497634'
+  }
+};

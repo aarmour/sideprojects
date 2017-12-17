35fa0e08e698cc3a17ab9315f8f30adb607d9ef7
Adam Armour <adam.armour@gmail.com>
2017-12-17 16:29:07 -0700

    build: add script to index trails for search
    packages: trails-app

---
+++ packages/trails-app/scripts/index-trails-for-search.ts
@@ -0,0 +1,39 @@
+const path = require('path');
+const algoliasearch = require('algoliasearch');
+const getStdin = require('get-stdin');
+
+interface Settings {
+  appId: string;
+  adminApiKey: string;
+}
+
+function main() {
+  let env: Settings = { appId: null, adminApiKey: null };
+  try { env = require('../.local/environment.local').environment.algolia; } catch (e) { }
+
+  const ALGOLIA_APP_ID = env.appId || process.env.ALGOLIA_APP_ID;
+  const ALGOLIA_ADMIN_API_KEY = env.adminApiKey || process.env.ALGOLIA_ADMIN_API_KEY;
+
+  const ALGOLIA_INDEX_NAME = 'trails';
+  const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_ADMIN_API_KEY);
+  const index = client.initIndex(ALGOLIA_INDEX_NAME);
+
+  getStdin()
+    .then((data: string) => JSON.parse(data).trails)
+    .then((trails: object) => Object.keys(trails)
+        .map((key: string) => {
+          const item = trails[key];
+          return { objectID: key, name: item.name, slug: item.slug };
+        }))
+    .then((objects) => {
+      index.addObjects(objects, function (err) {
+        if (err) {
+          console.error('Failed to index objects:');
+          console.error(err.stack);
+          process.exit(1);
+        }
+      });
+    });
+}
+
+main();
